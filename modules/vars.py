545835281a225c38c1b2eabd52272d71c3614b82
hemant67890 <godarasubhash337@gmail.com>
2024-11-19 21:00:32 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "20346550"
 api_hash = "bc79c3bea7a626887bdc0871eecf0327"
-bot_token = "6955629747:AAHo857hsQ1XrQyppnPkG6y0IUiPC0xei34"
+bot_token = "7510608040:AAE8gLvOPW-vqehLwN0qrEs5nryHrimEsbk"
